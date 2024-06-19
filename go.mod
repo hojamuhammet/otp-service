720019dbf8c5492b05cb38dc532e72fe51382706
kemvl <atdayewkemal@gmail.com>
2024-06-19 13:01:18 +0500

    sms sending logic by modem is implemented

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/go-chi/chi/v5 v5.0.12
 	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/ilyakaznacheev/cleanenv v1.5.0
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
 )
 
 require (
@@ -13,6 +14,7 @@ require (
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/onsi/ginkgo v1.16.5 // indirect
 	github.com/onsi/gomega v1.33.1 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
--- internal/service/smpp-otp-service.go
+++ internal/service/smpp-otp-service.go
@@ -6,7 +6,10 @@ import (
 	"otp-service/internal/config"
 	repository "otp-service/internal/repository/interfaces"
 	"otp-service/pkg/lib/logger"
+	"strings"
 	"time"
+
+	"github.com/tarm/serial"
 )
 
 type OTPService struct {
@@ -33,6 +36,56 @@ func (s *OTPService) SaveAndSendOTP(phoneNumber string) error {
 		return err
 	}
 
+	err = s.sendSMS(phoneNumber, otp)
+	if err != nil {
+		s.logger.ErrorLogger.Error("Error sending OTP via SMS: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func (s *OTPService) sendSMS(phoneNumber, otp string) error {
+	c := &serial.Config{Name: "COM14", Baud: 115200}
+	serialPort, err := serial.OpenPort(c)
+	if err != nil {
+		return fmt.Errorf("failed to open serial port: %w", err)
+	}
+	defer serialPort.Close()
+
+	if _, err := serialPort.Write([]byte("AT\r")); err != nil {
+		return fmt.Errorf("failed to initialize modem: %w", err)
+	}
+	time.Sleep(500 * time.Millisecond)
+
+	if _, err := serialPort.Write([]byte("AT+CMGF=1\r")); err != nil {
+		return fmt.Errorf("failed to set SMS text mode: %w", err)
+	}
+	time.Sleep(500 * time.Millisecond)
+
+	if _, err := serialPort.Write([]byte(fmt.Sprintf("AT+CMGS=\"%s\"\r", phoneNumber))); err != nil {
+		return fmt.Errorf("failed to specify recipient number: %w", err)
+	}
+	time.Sleep(500 * time.Millisecond)
+
+	message := fmt.Sprintf("Your OTP is: %s", otp)
+	if _, err := serialPort.Write([]byte(message + "\x1A")); err != nil {
+		return fmt.Errorf("failed to send message: %w", err)
+	}
+	time.Sleep(500 * time.Millisecond)
+
+	buf := make([]byte, 128)
+	n, err := serialPort.Read(buf)
+	if err != nil {
+		return fmt.Errorf("failed to read response: %w", err)
+	}
+
+	response := string(buf[:n])
+	if !strings.Contains(response, "+CMGS:") {
+		return fmt.Errorf("failed to send message, response: %s", response)
+	}
+
+	s.logger.InfoLogger.Info(fmt.Sprintf("Message 'Your OTP is: %s' sent to %s, status: %s\n", otp, phoneNumber, response))
 	return nil
 }
 
